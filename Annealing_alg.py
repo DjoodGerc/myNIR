19ef45b1db236ac4a9010b4055c993b59738019d
chmo_kudryavoe <avramenko.pav@yandex.ru>
2024-08-30 20:48:25 +0300

    done for summer 2024

--- Annealing_alg.py
+++ Annealing_alg.py
@@ -1,42 +1,42 @@
-import numpy as np
-
-from Graph_generation import Graph
-
-
-def annealing_algorithm(g:Graph,n_colors:int,t: float,rate_of_inrease)->dict:
-    """
-    Метод Отжига
-    """
-    g.coloring={i:np.random.randint(0,n_colors) for i in g.graph.keys()}
-    rs = np.random
-
-    #условие остановки
-
-    while 1/t>10**-10:
-
-        # Берем случайную вершину, берем случайный цвет оценивам разницу между ошибками со старым и новым цветом
-
-        vert = np.random.randint(0, len(g.graph.keys()))
-        new_color = (rs.randint(1, n_colors) + g.coloring[vert]) % n_colors
-        new_coloring = g.coloring.copy()
-        new_coloring[vert] = new_color
-
-        delta = g.get_d_obj(vert,new_color)[0]
-        print(delta)
-        print(1/t)
-        r = np.random.rand()
-
-        # если раскраска улучшилась : принимаем; нет: c вероятностью np.exp(-t * delta) принимаем
-
-        if delta <= 0:
-            g.coloring = new_coloring
-
-        elif r < np.exp(-t * delta):
-            
-            g.coloring = new_coloring
-
-
-        t *= rate_of_inrease
-        
-    return g.coloring
-
+# import numpy as np
+#
+# from Graph_generation import Graph
+#
+#
+# def annealing_algorithm(g:Graph,n_colors:int,t: float,rate_of_inrease)->dict:
+#     """
+#     Метод Отжига
+#     """
+#     g.coloring={i:np.random.randint(0,n_colors) for i in g.graph.keys()}
+#     rs = np.random
+#
+#     #условие остановки
+#
+#     while t>10**-3:
+#
+#         # Берем случайную вершину, берем случайный цвет оценивам разницу между ошибками со старым и новым цветом
+#
+#         vert = np.random.randint(0, len(g.graph.keys()))
+#         new_color = (rs.randint(1, n_colors) + g.coloring[vert]) % n_colors
+#         new_coloring = g.coloring.copy()
+#         new_coloring[vert] = new_color
+#
+#         delta = g.get_d_obj(vert,new_color)[0]
+#         print(delta)
+#         print(1/t)
+#         r = np.random.rand()
+#
+#         # если раскраска улучшилась : принимаем; нет: c вероятностью np.exp(-t * delta) принимаем
+#
+#         if delta <= 0:
+#             g.coloring = new_coloring
+#
+#         elif r < np.exp(-t * delta):
+#
+#             g.coloring = new_coloring
+#
+#
+#         t *= rate_of_inrease
+#
+#     return g.coloring
+#
--- Graph_generation.py
+++ Graph_generation.py
@@ -1,7 +1,10 @@
 import numpy as np
+
+
 # from plots import show_graph
 
 class Graph:
+    rs = np.random.RandomState(42)
     coloring = {}
     graph = {}
 
@@ -48,19 +51,19 @@ class Graph:
         """
             генерация графа
         """
-        rs = np.random.RandomState(125156)
+
         graph = {i: {} for i in range(n_vertice)}
 
         n_edges = mean_degree * n_vertice / 2
         while n_edges > 0:
 
             while True:
-                v1 = rs.randint(n_vertice)
-                v2 = (rs.randint(1, n_vertice) + v1) % n_vertice
+                v1 = self.rs.randint(n_vertice)
+                v2 = (self.rs.randint(1, n_vertice) + v1) % n_vertice
                 if not v2 in graph[v1]:
                     break
 
-            weight = rs.random()
+            weight = self.rs.random()
 
             graph[v1][v2] = weight
             graph[v2][v1] = weight
@@ -71,12 +74,12 @@ class Graph:
 
         return graph
 
-    def random_coloring(self, n_colors: int, seed: int):
-        rs = np.random.RandomState(seed)
-        return {i: rs.randint(n_colors) for i in self.graph}
+    def random_coloring(self, n_colors: int):
+
+        return {i: self.rs.randint(n_colors) for i in self.graph}
 
     def create_err_table(self, n_colors):
-        #get obj
+        # get obj
         g = self.graph
         err_table = []
 
@@ -97,52 +100,155 @@ class Graph:
         old_color = self.coloring[vertice]
 
         for i in neighbors:
-            err_table[i][old_color] -=self.graph[vertice][i]
+            err_table[i][old_color] -= self.graph[vertice][i]
             err_table[i][new_color] += self.graph[vertice][i]
         return err_table
+    def find_min_not_eq_this(self,delta_arr,new_colors):
+        this_min_not_eq=delta_arr[0]
+        tmine_arg=None
+        if new_colors[0]==self.coloring[0]:
+            q_arg=0
+        else:
+            q_arg=None
+        for i in range(1,len(delta_arr)):
+            if new_colors[i]==self.coloring[i]:
+                q_arg=i
+            else:
+                if delta_arr[i]<this_min_not_eq:
+                    this_min_not_eq=delta_arr[i]
+                    tmine_arg=i
+        return tmine_arg, this_min_not_eq,q_arg
 
-    def hill_climbing(self, n_colors: int):
+    def hill_climbing(self, n_colors: int,seed=77):
 
         g = self.graph
-        self.coloring = self.random_coloring(n_colors, 2567)
+        self.coloring = self.random_coloring(n_colors)
+        fst_o=self.get_obj(self.coloring)
         err_table = self.create_err_table(n_colors)
         n_vertices = len(g)
         k = 0
+
+        obj_array = []
+        obj_array.append(self.get_obj(self.coloring))
+
         while True:
 
             new_colors = np.argmin(err_table, axis=1)
             delta_array = err_table[np.arange(n_vertices), new_colors] - err_table[
                 np.arange(n_vertices), list(self.coloring.values())]
             v_candidate = np.argmin(delta_array)
+
+
             d = delta_array[v_candidate]
             selected_color = new_colors[v_candidate]
 
-            if d >= 0 or k > 50000:
-
+            if d >= 0 :#or k > 50000:
                 break
-
-
+            fst_o+=d
             err_table = self.change_err_table(err_table, v_candidate, selected_color)
             self.coloring[v_candidate] = selected_color
 
             k += 1
-        return self.coloring
+            obj_array.append(obj_array[len(obj_array)-1]+d)
+            print(k)
+        # print(fst_o)
+        return self.coloring,obj_array
 
     def annealing(self, n_colors: int, t: float, inc: float):
         g = self.graph
-        self.coloring = self.random_coloring(n_colors, 12453)
+        self.coloring = self.random_coloring(n_colors)
         err_table = self.create_err_table(n_colors)
         n_vertices = len(g)
         # print(g)
         # print(err_table)
+        obj_array=[]
+        obj_array.append(self.get_obj(self.coloring))
+        temp_array=[]
+        # print(err_table)
 
-        while 1 / t > 10 ** -6:
+        while t > 10 ** -5:
 
             # new_colors = np.argmin(err_table, axis=1)
-            new_colors=[]
-            len_arr=len(err_table[0])
+            new_colors = []
+            len_arr = len(err_table[0])
             for i in range(len(err_table)):
-                new_colors.append(np.random.randint(len_arr))
+                # new_colors.append((np.random.randint(1,len_arr)+self.coloring[i])%len_arr)
+                new_colors.append(self.rs.randint(len_arr))
+
+            # кол-во соседей нового цвета - кол-во соседей старого цвета
+            # print(new_colors)
+            delta_array = err_table[np.arange(n_vertices), new_colors] - err_table[
+                np.arange(n_vertices), list(self.coloring.values())]
+
+            # ?
+            # v_candidate = np.random.randint(len(delta_array))
+
+            # v_candidate = np.argmin(delta_array)
+            fmne = self.find_min_not_eq_this(delta_array,new_colors)
+            # print(fmne)
+            if fmne[0] is None:
+                v_candidate = fmne[2]
+            elif fmne[1]<0:
+                # print(v_candidate==fmne[0])
+                v_candidate=fmne[0]
+            elif fmne[2] is None :
+                v_candidate = fmne[0]
+            else:
+                r=self.rs.random()
+                if r < np.exp(-fmne[1]/t):
+
+                    v_candidate=fmne[0]
+                else:
+                    v_candidate=fmne[2]
+
+            if v_candidate is None:
+                v_candidate=0
+            d = delta_array[v_candidate]
+
+            r = self.rs.random()
+            # print(fmne)
+
+            selected_color = new_colors[v_candidate]
+
+            if d < 0 or r < np.exp(-d/t):
+                err_table = self.change_err_table(err_table, v_candidate, selected_color)
+                self.coloring[v_candidate] = selected_color
+                obj_array.append(obj_array[len(obj_array)-1]+d)
+            else:
+                obj_array.append(obj_array[len(obj_array)-1])
+
+            # print(new_colors)
+            # if(d>=0):
+            #     print(f"d: {d}")
+            #     print(f"exp: {np.exp(-d/t)}")
+            t /= inc
+            print(f"fst: {t}")
+
+
+        return self.coloring,obj_array
+
+
+
+    def annealing_snd(self, n_colors: int, t: float, inc: float,seed=77):
+        g = self.graph
+        self.coloring = self.random_coloring(n_colors)
+        err_table = self.create_err_table(n_colors)
+        n_vertices = len(g)
+        # print(g)
+        # print(err_table)
+        obj_array=[]
+        obj_array.append(self.get_obj(self.coloring))
+        temp_array=[]
+        # print(err_table)
+
+        while t > 10 ** -5:
+
+            # new_colors = np.argmin(err_table, axis=1)
+            new_colors = []
+            len_arr = len(err_table[0])
+            for i in range(len(err_table)):
+                new_colors.append((self.rs.randint(1,len_arr)+self.coloring[i])%len_arr)
+                # new_colors.append(np.random.randint(len_arr))
 
             # кол-во соседей нового цвета - кол-во соседей старого цвета
             # print(new_colors)
@@ -153,19 +259,31 @@ class Graph:
             # v_candidate = np.random.randint(len(delta_array))
 
             v_candidate = np.argmin(delta_array)
-            d = delta_array[v_candidate]
-            r = np.random.rand()
-            selected_color = new_colors[v_candidate]
 
+
+            d = delta_array[v_candidate]
             # print(d)
-            if d <= 0 or r < np.exp(-t * d):
+            # print(t)
+            # print(delta_array)
+            r = self.rs.random()
+            # print(v_candidate)
+            selected_color = new_colors[v_candidate]
+            # print(selected_color==self.coloring[v_candidate])
+            # print(d)
+            if d < 0 or r < np.exp(-d/t):
                 err_table = self.change_err_table(err_table, v_candidate, selected_color)
                 self.coloring[v_candidate] = selected_color
+                obj_array.append(obj_array[len(obj_array) - 1] + d)
+            else:
+                obj_array.append(obj_array[len(obj_array) - 1])
+            # print(new_colors)
+            # if(d>=0):
+            #     print(f"d: {d}")
+            #     print(f"exp: {np.exp(-d/t)}")
+            t /= inc
+            print(f"snd: {t}")
+        return self.coloring,obj_array
 
-
-            t *= inc
-
-        return self.coloring
 
 
 if __name__ == '__main__':
@@ -173,7 +291,7 @@ if __name__ == '__main__':
 
     # a.create_err_table(5)
 
-    print(a.hill_climbing(3))
+    # print(a.hill_climbing(3))
     # a.random_coloring(5)
 
     # print(check(graph_generation(3, 33)))
